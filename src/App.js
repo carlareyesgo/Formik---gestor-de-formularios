7adbff32f816b38400cea8cbc1931a885086de04
Carla Godínez <carlareyesgo@gmail.com>
2023-05-31 16:17:31 -0700

    Componente Formik

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useFormik } from "formik";
+import { Formik } from "formik";
 
 const validate = (values) => {
   const errors = {}
@@ -16,31 +16,34 @@ const validate = (values) => {
   return errors
 }
 function App() {
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      lastname: '',
-      email: ''
-    },
-    validate,
-    onSubmit: values => console.log(values)
-  })
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label>Nombre</label>
-      <input type="text" {...formik.getFieldProps('name')}/>
-      {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div> : null}
-      <br />
-      <label>Apellido</label>
-      <input type="text"{...formik.getFieldProps('lastname')}/>
-      {formik.touched.lastname && formik.errors.lastname ? <div>{formik.errors.lastname}</div> : null}
-      <br />
-      <label>Correo</label>
-      <input type="email" {...formik.getFieldProps('email')}/>
-      {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
-      <br />
-      <button type="submit">Enviar</button>
-    </form>
+    <Formik
+      initialValues={{
+        name: '',
+        lastname: '',
+        email: ''
+      }}
+      validate={validate}
+      onSubmit={values => console.log(values)}
+    >
+      {formik =>
+        <form onSubmit={formik.handleSubmit}>
+          <label>Nombre</label>
+          <input type="text" {...formik.getFieldProps('name')} />
+          {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div> : null}
+          <br />
+          <label>Apellido</label>
+          <input type="text"{...formik.getFieldProps('lastname')} />
+          {formik.touched.lastname && formik.errors.lastname ? <div>{formik.errors.lastname}</div> : null}
+          <br />
+          <label>Correo</label>
+          <input type="email" {...formik.getFieldProps('email')} />
+          {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
+          <br />
+          <button type="submit">Enviar</button>
+        </form>
+      }
+    </Formik>
   );
 }
 
