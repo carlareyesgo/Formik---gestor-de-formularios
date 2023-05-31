b0c73527bd9361175b5b7a5afd18932622df6ae7
Carla Godínez <carlareyesgo@gmail.com>
2023-05-31 16:01:38 -0700

    Agregando validaciones

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import { useFormik } from "formik";
 
+const validate = (values) => {
+  const errors = {}
+  if (!values.name) {
+    errors.name = 'Requerido'
+  } else if (values.name.length < 5) {
+    errors.name = 'El nombre es muy corto'
+  }
+  if (!values.lastname) {
+    errors.lastname = 'Requerido'
+  } else if (values.lastname.length < 5) {
+    errors.lastname = 'El apellido es muy corto'
+  }
+
+  return errors
+}
 function App() {
   const formik = useFormik({
     initialValues: {
@@ -7,18 +22,23 @@ function App() {
       lastname: '',
       email: ''
     },
+    validate,
     onSubmit: values => console.log(values)
   })
   return (
-   <form onSubmit={formik.handleSubmit}>
+    <form onSubmit={formik.handleSubmit}>
       <label>Nombre</label>
-      <input name="name" type="text" onChange={formik.handleChange} value={formik.values.name}/>
+      <input name="name" type="text" onChange={formik.handleChange} value={formik.values.name} />
+      {formik.errors.name ? <div>{formik.errors.name}</div> : null}
+      <br />
       <label>Apellido</label>
-      <input name="lastname" type="text" onChange={formik.handleChange} value={formik.values.lastname}/>
+      <input name="lastname" type="text" onChange={formik.handleChange} value={formik.values.lastname} />
+      {formik.errors.lastname ? <div>{formik.errors.lastname}</div> : null}
+      <br />
       <label>Correo</label>
-      <input name="email" type="email" onChange={formik.handleChange} value={formik.values.email}/>
-      <button>Enviar</button>
-   </form>
+      <input name="email" type="email" onChange={formik.handleChange} value={formik.values.email} />
+      <button type="submit">Enviar</button>
+    </form>
   );
 }
 
